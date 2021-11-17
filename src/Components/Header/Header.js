d93d5eaecf72dfeaab4a17cac8ad2affe74cdf41
Sanket Bagad <alencolins@gmail.com>
2021-11-17 11:33:12 +0530

    Working on all components

---
+++ src/Components/Header/Header.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <p><i class="fas fa-exclamation-triangle"></i> CT use management case is down due to unexpected server crashing, the expected..</p>
+        </div>
+    )
+}
+
+export default Header
